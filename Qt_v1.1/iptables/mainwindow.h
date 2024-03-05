6ffab67289874024850bfd9026605912f78e2c44
IANTSOGod <iantsochristianrazafindrazaka@gmail.com>
2024-03-05 08:59:31 +0100

    v1.1

--- Qt_v1.1/iptables/mainwindow.h
+++ Qt_v1.1/iptables/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <vector>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -14,6 +15,17 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
+    std::vector<QString> list_cmd;
+    QString typeTarget;
+    QString choicePolicy;
+    QString typePort;
+    QString portValue;
+    QString typeDNS;
+    QString adress;
+    QString typeMac;
+    QString macAdress;
+    QString cmd;
+    QString text="iptables -A";
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
@@ -45,6 +57,34 @@ private slots:
     void on_pushButton_14_clicked();
 
     void on_pushButton_7_clicked();
+
+    void on_pushButton_13_clicked();
+
+    void on_radioButton_clicked();
+
+    void on_radioButton_2_clicked();
+
+    void on_radioButton_3_clicked();
+
+    void on_pushButton_17_clicked();
+
+    void on_pushButton_18_clicked();
+
+    void on_pushButton_19_clicked();
+
+    void on_pushButton_20_clicked();
+
+    void on_pushButton_21_clicked();
+
+    void on_pushButton_22_clicked();
+
+    void on_pushButton_24_clicked();
+
+    void on_pushButton_25_clicked();
+
+    void on_pushButton_23_clicked();
+
+    void on_pushButton_3_clicked();
 
 private:
     Ui::MainWindow *ui;
